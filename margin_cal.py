e002a3f842c7af4b9220631d993226a747d217b8
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-02 12:19:45 +0800

    Update margin_cal.py

--- margin_cal.py
+++ margin_cal.py
@@ -40,8 +40,5 @@ def excel_file(name):
 df_oc = excel_file('OC Sales Order Enquiry')
 df_cost = excel_file('Cost Excel File')
 
-df_merge = pd.merge(df_oc , df_cost, on='Model', how='outer', indicator=True)
-df_both = df_merge[df_merge['_merge'] == 'both']
-df_left = df_merge[df_merge['_merge'] == 'left_only']
-
+df_merge = pd.merge(df_oc , df_cost, on='Model', how='left)
 df_merge
