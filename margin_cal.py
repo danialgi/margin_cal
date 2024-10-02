692acd475bef81aaef054f1fe39e053240d88e16
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-02 12:02:07 +0800

    Create margin_cal.py

---
+++ margin_cal.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import webbrowser as wb
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+from io import BytesIO
+import calendar
+from datetime import datetime, date, time
+current_datetime = datetime.now()
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import math
+
+st.set_page_config(page_title="Margin Calulator", page_icon="🧮", layout="wide")
+st.write("🏢 Goh Office Supplies")
+st.title("Margin Calculator🧮")
+st.markdown("#")
+
+def excel_file(name):
+    file = st.file_uploader(name,type=['xlsx'])
+    if not file:
+        st.stop()
+    df_cart = pd.read_excel(cart_file)
+
+    #rows_to_take = second_column[second_column.iloc[:, 0] == "Marketplace Order No."].index[0]
+    #df_cart = df_cart.iloc[rows_to_take:]
+    #df_cart.columns = df_cart.iloc[0]
+    #df_cart = df_cart.drop([rows_to_take])
+    
+    df_cart['Order ID'] = df_cart['Order ID'].ffill()
+    df_cart['Date Added'] = df_cart['Date Added'].ffill()
+    if 'Order Source' in df_cart.columns:
+        df_cart['Order Source'] = df_cart['Order Source'].ffill()
+    df_cart['Order Status'] = df_cart['Order Status'].ffill()
+    df_cart['Delivery Method'] = df_cart['Delivery Method'].ffill()
+    if 'Tracking' in df_cart.columns:
+        df_cart['Tracking'] = df_cart['Tracking'].ffill()
+    if 'Tracking No.' in df_cart.columns:
+        df_cart['Tracking No.'] = df_cart['Tracking No.'].ffill()
+    df_cart['Courier'] = df_cart['Courier'].ffill()
+
+    return df_cart
+
+df_oc = excel_file('OC Sales Order Enquiry')
+df_oc
+
+    
