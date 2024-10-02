50a185a7a422a9bb945064f17faf06efdcdb766a
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-02 12:25:43 +0800

    Update margin_cal.py

--- margin_cal.py
+++ margin_cal.py
@@ -41,4 +41,5 @@ df_oc = excel_file('OC Sales Order Enquiry')
 df_cost = excel_file('Cost Excel File')
 
 df_merge = pd.merge(df_oc , df_cost, on='Model', how='left')
+df_merge['New Margin'] = df_merge['Order Income By Item'] - (df_merge['Cost']*df_merge['Quantity'])
 df_merge
