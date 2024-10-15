a40e635b863ed9963f4f1224fd0981dab146c60e
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-15 11:04:57 +0800

    Update margin_cal.py

--- margin_cal.py
+++ margin_cal.py
@@ -65,7 +65,7 @@ df_merge = pd.merge(df_oc , df_cost, on='Model', how='left')
 #df_merge['Order Income By Item'] = pd.to_numeric(df_merge['Quantity'], errors='coerce')
 #df_merge['Cost'] = pd.to_numeric(df_merge['Quantity'], errors='coerce')
 #df_merge['Quantity'] = pd.to_numeric(df_merge['Quantity'], errors='coerce')
-#df_merge['New Margin'] = df_merge['Order Income By Item'] - (df_merge['Cost']*df_merge['Quantity'])
+df_merge['New Margin'] = df_merge['Order Income By Item'] - (df_merge['Cost']*df_merge['Quantity'])
 df_merge
 
 "#"
