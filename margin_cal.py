e49262d3d6deaddb965dbf6ffe0f348ed8502195
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-15 10:30:50 +0800

    Update margin_cal.py

--- margin_cal.py
+++ margin_cal.py
@@ -35,7 +35,7 @@ def excel_file(name):
         df_cart.columns = df_cart.iloc[0]
         df_cart = df_cart.drop([rows_to_take])
         
-    df_cart ['Model'] = df_cart ['Model'].str.strip()
+    #df_cart ['Model'] = df_cart ['Model'].str.strip()
     #df_cart
     return df_cart
 
