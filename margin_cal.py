9f5a977a17699673938244540cfd575578117034
danialgi <163375828+danialgi@users.noreply.github.com>
2024-10-15 10:59:41 +0800

    Update margin_cal.py

--- margin_cal.py
+++ margin_cal.py
@@ -63,8 +63,7 @@ df_cost = excel_file('Cost Excel File')
 "RESULTS: "
 df_oc['Model'] = df_oc['Model'].str.strip()
 df_merge = pd.merge(df_oc , df_cost, on='Model', how='left')
-df_merge['Cost'].dtype
-df_merge['Quantity'].dtype
+df_merge
 df_merge['New Margin'] = df_merge['Order Income By Item'] - (df_merge['Cost']*df_merge['Quantity'])
 df_merge
 
